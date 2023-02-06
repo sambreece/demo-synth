16046020a88ee904b5d5ae9231e6d1702bdcfed9
sambreece <Samantha.Breece@tlgcohort.com>
2023-02-05 19:41:08 -0800

    first attempt with synth APIs

---
+++ js/main.js
@@ -0,0 +1,90 @@
+
+//Music Visualizer:
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth/2, window.innerHeight/2);
+cube_container.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
+
+
+//Speech Synthesizer:
+
+    //Populating the voice selector
+function populateVoiceList() {
+  if (typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voiceSelect").appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+    // Text-To Speech Setup:
+speechForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const msg = useFormInputs(event);
+    speechSynthesis.speak(msg);
+});
+
+function useFormInputs(event) {
+    let msg = new SpeechSynthesisUtterance();
+    const voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[voiceSelect.selectedIndex];
+    msg.text = speechText.value;
+    msg.rate = rateRange.value;
+    msg.pitch = pitchRange.value;
+    msg.volume = volumeRange.value;
+    return msg;
+}
+
+//Tone.js:
+
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+function playNote() {
+    const now = Tone.now();
+    synth.triggerAttackRelease("C4", "8n", now);
+    synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    synth.triggerAttackRelease("G4", "8n", now + 1);
+}
+
+
+startSynth.addEventListener("click", () => 
+playNote()
+)
+
